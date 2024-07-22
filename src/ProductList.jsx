4dceeec2c82634a9f7d39b34ab498ab09a0ca468
ponces95 <129859289+ponces95@users.noreply.github.com>
2024-07-22 17:53:01 -0600

    Task 6

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,28 +1,10 @@
 import React, { useState,useEffect } from 'react';
 import {useDispatch , useSelector} from 'react-redux';
-import {addItem, selectTotalItems} from './CreatSlice';
+import {addItem} from './CreatSlice';
 import './ProductList.css'
 import Cart from './CartItem';
 
 function ProductList() {
-    const [showCart, setShowCart] = useState(false); 
-    const [addedToCart, setAddedToCart] = useState({}); // State to control the visibility of the About Us page
-    const dispatch = useDispatch();
-    const totalItems = useSelector(selectTotalItems);
-
-    const handleCart = () => {
-        setShowCart(true);
-    };
-const handleProducts = () =>  {
-    setShowCart(false);
-};
-  const handleAddToCart = (product) => {
-    dispatch(addItem(product));
-    setAddedToCart((prevState) => ({
-        ...prevState,
-        [product.name]: true,                                       //Set the product name as key and value as true to indicate it's added to cart
-    }));
-  };
     const plantsArray = [
         {
             category: "Air Purifying Plants",
@@ -250,23 +232,29 @@ const handleProducts = () =>  {
     fontSize: '30px',
     textDecoration: 'none',
    }
-   const handleCartClick = (e) => {
-    e.preventDefault();
-    setShowCart(true); // Set showCart to true when cart icon is clicked
-};
-const handlePlantsClick = (e) => {
-    e.preventDefault();
-    setShowPlants(true); // Set showAboutUs to true when "About Us" link is clicked
-    setShowCart(false); // Hide the cart when navigating to About Us
-};
-
-   const handleContinueShopping = (e) => {
-    e.preventDefault();
-    setShowCart(false);
-  };
-    return (
+    const [showCart, setShowCart] = useState(false); 
+    const [addedToCart, setAddedToCart] = useState({}); // State to control the visibility of the About Us page
+    const dispatch = useDispatch();
+   
+    const handleAddToCart = (product) => {
+        dispatch(addItem(product));
+        setAddedToCart((prevState) => ({
+            ...prevState,
+            [product.name]: true,                                       //Set the product name as key and value as true to indicate it's added to cart
+            }));
+    };
+    const cartItems = useSelector((state)=> state.cart.items);
+    const totalQuantity = () => {
+        let total =0 ;
+        cartItems.foreach(item =>{
+            total +=item.quantity;
+        });
+        return total;
+    };
+    
+ return (
         <div>
-             <div className="navbar" style={styleObj}>
+            <div className="navbar" style={styleObj}>
             <div className="tag">
                <div className="luxury">
                <img src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_1280.png" alt="" />
@@ -280,18 +268,43 @@ const handlePlantsClick = (e) => {
               
             </div>
             <div style={styleObjUl}>
-                <div> <a href="#" onClick={(e)=>handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+                <div> <a href="#" style={styleA} onClick={()=>setShowCart(false)}>Plants</a></div>
+                <div> 
+                    <a href="#" style={styleA} onClick={()=>setShowCart(true)}>
+                        <h1 className='cart'>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68">
+                                <rect width="156" height="156" fill="none"></rect>
+                                <circle cx="80" cy="216" r="12"></circle>
+                                <circle cx="184" cy="216" r="12"></circle>
+                                <path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path>
+                            </svg>
+                            {totalQuantity() > 0 && <span className='cart_quantity_count'>{totalQuantity()}</span>}
+                        </h1>                    
+                    </a>
+                </div>
             </div>
         </div>
-        {!showCart? (
-        <div className="product-grid">
 
-
-        </div>
- ) :  (
-    <CartItem onContinueShopping={handleContinueShopping}/>
-)}
+        {showCart ? (
+        <Cart onContinueShopping={()=>setShowCart(false)} />
+        ) : (
+            <div className="product-grid">
+                {plantsArray.map((category, index) => (
+                    <div key={index}>
+                        <h1><div>{category.category}</div></h1>
+                        <div className="product-list">
+                        {category.plants.map((plant, plantIndex) => (
+                            <div className="product-card" key={plantIndex}>
+                            <img className="product-image" src={plant.image} alt={plant.name} />
+                            <div className="product-title">{plant.name}</div>
+                            <button  onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+                            </div>
+                        ))}
+                        </div>
+                    </div>
+                ))}
+            </div>
+        )}
     </div>
     );
 }
