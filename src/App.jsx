d75de1a9893e4aac54158e83c134ec5ace925746
ponces95 <129859289+ponces95@users.noreply.github.com>
2024-07-22 16:39:08 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
 
 import React, { useState } from 'react';
 import ProductList from './ProductList';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import AboutUs from './AboutUs';
+import Cart from './CartItem';
+import ProductList from './Productlist';
+import store from './store';
+import { Provider } from 'react-redux';
 
 function App() {
   
   const [showProductList, setShowProductList] = useState(false);
+  const [showCart, setShowCart] = useState (false);
 
   const handleGetStartedClick = () => {
     setShowProductList(true);
   };
+  const handleCart = () => {
+    setShowCart(true);
+  };
+const handleProducts = () =>  {
+    setShowCart(false);
+};
+  
 
   return (
     <div className="app-container">
