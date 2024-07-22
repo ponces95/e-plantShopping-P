71a2545461479876f089fb67b035f6028218562d
ponces95 <129859289+ponces95@users.noreply.github.com>
2024-07-22 17:29:37 -0600

    Task 2

--- src/CreatSlice.jsx
+++ src/CreatSlice.jsx
@@ -4,7 +4,6 @@ export const CreatSlice = createSlice({
   name: 'cart',
   initialState: {
     items: [], // Initialize items as an empty array
-    totalItems:0
   },
   reducers: {
     addItem: (state, action) => {
@@ -15,26 +14,20 @@ export const CreatSlice = createSlice({
       } else {
         state.items.push({name, image, cost, quantity : 1 });
       }
-    state.totaItems++;
     },
     removeItem: (state, action) => {
-      const {name} = action.payload;
-      const itemToRemove = state.items.find(item=> item.name===name);
-      if (itemToRemove){
-        state.totalItems -= itemToRemove.quantity;
-        state.items = state.items.filter(item => item.name !== name);
+        state.items = state.items.filter(item => item.name !== action.payload);
     },
     updateQuantity: (state, action) => {
       const {name, quantity } = action.payload;
       const itemToUpdate = state.items.find(item => item.name === name);
       if (itemToUpdate) {
-        state.totalItems += (quantity - itemToUpdate.quantity);
         itemToupdate.quantity = quantity;
       }
     },
   },
 });
-export const selectTotalItems = state => state.cart.totalItems;
+
 export const { addItem, removeItem, updateQuantity } = CreatSlice.actions;
 
 export default CreatSlice.reducer;
